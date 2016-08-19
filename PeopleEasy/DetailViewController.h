27cf9e578b0c96d078832d81dc1a9bd3d5584b48
Manish Singh <manish.singh705@gmail.com>
2016-08-19 17:40:56 +0530

    Detialview controller

---
+++ PeopleEasy/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  PeopleEasy
+//
+//  Created by Manish Singh on 8/19/16.
+//  Copyright © 2016 Esfera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
